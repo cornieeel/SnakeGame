d477c5baf54c4a383dc7abb2ca26de25491ed310
Adam Horelican <horelicanadam@gmail.com>
2025-11-20 11:09:21 +0100

    minor changes

--- html-js-name/snake game/script.js
+++ html-js-name/snake game/script.js
@@ -4,6 +4,9 @@ const totalCells = Math.pow(boardSize, 2);
 const resetButton = document.getElementById('resetBtn');
 let score = document.getElementById("score");
 score.innerHTML = 0;
+let snake = [];
+let direction = 1;
+
 
 const cells = [];
 for (let i = 0; i < totalCells; i++) {
@@ -15,9 +18,10 @@ for (let i = 0; i < totalCells; i++) {
 
 const snakeLength = 1;
 
+
+document.addEventListener("keydown", changeDirection);
+
 startGame();
-
-
 
 
 function clearBoard(){
@@ -25,12 +29,19 @@ function clearBoard(){
 }
 
 function startGame(){
+
   let start = Math.floor(Math.random() * totalCells);
-let snake = [start];
-let food = Math.floor(Math.random() * totalCells);
-while (food === start) {
+  snake = [start];
+
+  let food = Math.floor(Math.random() * totalCells);
   food = Math.floor(Math.random() * totalCells);
-} 
+  tick();
+
+  
+
+
+
+
 snake.forEach(i => {
   if (i >= 0 && i < totalCells) {
     cells[i].classList.add('snake');
@@ -52,22 +63,55 @@ function tick(){
   setTimeout(()=>{
     clearBoard();
     drawSnake();
-    growSnake();
     checkPositionOfFood();
+    checkIfGameOver();
+    moveHead();
     tick();
   },100);
 }
 
-function checkPositionOfFood(){}
+function changeDirection(event){
+    const keyPressed = event.keyCode;
+    const LEFT = 37;
+    const UP = 38;
+    const RIGHT = 39;
+    const DOWN = 40;
+
+    if (keyPressed === LEFT) direction = -1;
+    if (keyPressed === RIGHT) direction =1;
+    if (keyPressed === UP) direction = boardSize;
+    if (keyPressed === DOWN) direction = -boardSize;
+}
+
+function moveHead(){
+  let head = snake[0];
+}
+function generateFood(){
+  //need to check if fruit is not in body of snake, then generate fruit.
+  let food = Math.floor(Math.random() * totalCells);
+  food = Math.floor(Math.random() * totalCells);
+
+}
+function checkPositionOfFood(){
+  if (snake[0]== food){
+      generateFood();
+      growSnake();
+      score.textContent = parseInt(score.textContent,10)+1;
+  }
+  
+}
+
 function growSnake(){
 
 }
-function drawSnake(){}
 
-
-
+function checkIfGameOver(){
+  //If head is outside of borders or in the snake;
+  //gameover();
+}
+function gameover(){
+  score.textContent = 0;
+}
+function drawSnake(){
   
-    
-    
-
-
+}
